f43e183025548343229e262c4410607980d5cfd0
engrharisijaz <92294452+engrharisijaz@users.noreply.github.com>
2023-09-15 18:16:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import openai
 import streamlit as st
 
 st.title("FemGPT")
-         
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+openai.api_key = 
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "ft:gpt-3.5-turbo-0613:personal::7z0mfE8C"
